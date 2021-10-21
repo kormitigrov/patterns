6e0a06ea3485ce932f33bca5671d3fbb4f91cb03
Grigory Makeev <greg@ufanipi.ru>
2021-10-21 09:25:33 +0500

    переименования

--- cpp/prototype/prototype.cpp
+++ cpp/prototype/prototype.cpp
@@ -2,48 +2,46 @@
 
 using namespace std;
 
-class CAnimal
+class Animal
 {
 public:
 	// virtual function clone, which clones nothing
-	virtual CAnimal *clone() = 0;
-	virtual ~CAnimal()
+	virtual Animal *clone() = 0;
+	virtual ~Animal() { };
+};
+
+class Dog : public Animal
+{
+public:
+	// virtual function clone, which returns a copy of Dog
+	Animal *clone() override
 	{
+		cout << "Dog::clone()\n";
+		return new Dog();
 	}
 };
 
-class CDog : public CAnimal
+class Cat : public Animal
 {
 public:
-	// virtual function clone, which returns a copy of CDog
-	virtual CAnimal *clone()
+	// virtual function clone, which returns a copy of Cat
+	Animal *clone() override
 	{
-		cout << "CDog::clone()\n";
-		return new CDog();
-	}
-};
-
-class CCat : public CAnimal
-{
-public:
-	// virtual function clone, which returns a copy of CCat
-	virtual CAnimal *clone()
-	{
-		cout << "CCat::clone()\n";
-		return new CCat();
+		cout << "Cat::clone()\n";
+		return new Cat();
 	}
 };
 
 void main()
 {
 	// creating prototypes
-	CAnimal *cage[2] = {new CCat(), new CDog()};
+	Animal *cage[2] = {new Cat(), new Dog()};
 
 	// some time after ... we do not know which concrete object
 	// resides in which cage, but we can create a copy of each
 
-	CAnimal *newAnimal1 = cage[0]->clone(); // CCat created
-	CAnimal *newAnimal2 = cage[1]->clone(); // CDog created
+	Animal *newAnimal1 = cage[0]->clone(); // Cat created
+	Animal *newAnimal2 = cage[1]->clone(); // Dog created
 
 	delete newAnimal1;
 	delete newAnimal2;
--- cpp/template_method2/template_method2.cpp
+++ cpp/template_method2/template_method2.cpp
@@ -27,10 +27,21 @@ public:
 		printf("5\n");
 	}
 };
-
+class BeforeZ
+{
+public:
+	void TemplateMethod()
+	{
+		printf("1\n");
+		printf("2\n");
+		printf("!!!\n");
+		printf("4\n");
+		printf("5\n");
+	}
+};
 // решение проблемы повторяемого кода - вынесение шаблонного метода в 
 // родительский класс и выделение изменяемой части в отдельный метод
-class AfterZ
+class AfterBase
 {
 public:
 	virtual void doSpecific()
@@ -40,25 +51,33 @@ public:
 	{
 		printf("1\n");
 		printf("2\n");
-		doSpecific();
+		this->doSpecific();
 		printf("4\n");
 		printf("5\n");
 	}
 };
-class AfterX : public AfterZ
+class AfterX : public AfterBase
 {
 public:
-	void doSpecific()
+	void doSpecific() override
 	{
 		printf("hello\n");
 	}
 };
-class AfterY : public AfterZ
+class AfterY : public AfterBase
 {
 public:
-	void doSpecific()
+	void doSpecific() override
 	{
 		printf("world\n");
+	}
+};
+class AfterZ : public AfterBase
+{
+public:
+	void doSpecific() override
+	{
+		printf("!!!\n");
 	}
 };
 
@@ -67,19 +86,25 @@ void main()
 	printf("\nBEFORE\n");
 	BeforeX *bx = new BeforeX();
 	BeforeY *by = new BeforeY();
+	BeforeZ *bz = new BeforeZ();
 	bx->TemplateMethod();
 	by->TemplateMethod();
+	bz->TemplateMethod();
 
 	printf("\nAFTER\n");
 	AfterX *ax = new AfterX();
 	AfterY *ay = new AfterY();
+	AfterZ *az = new AfterZ();
 	ax->TemplateMethod();
 	ay->TemplateMethod();
+	az->TemplateMethod();
 
 	delete bx;
 	delete by;
+	delete bz;
 	delete ax;
 	delete ay;
+	delete az;
 
 	system("pause");
 }
